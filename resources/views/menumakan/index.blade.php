e7ef590bd5619ea3ff61978e891ac28c7d2949a4
Samuel Prayoga Tampubolon <samueltampubolon2002@gmail.com>
2023-03-10 16:22:19 +0700

    finalisasi iterasi 1

--- resources/views/menumakan/index.blade.php
+++ resources/views/menumakan/index.blade.php
@@ -55,5 +55,28 @@
             $('#example').DataTable();
         });
     </script>
-@include('partials.footer')
+<style>
+    #footer {
+        position:absolute;
+        bottom: 0;
+        width: 100%;
+        height: 60px;
+        /* Height of the footer */
+        background: #00337C;
+        font-family: sans-serif
+    }
+</style>
+
+<footer class="text-center text-lg-start bg-light text-muted" id="footer">
+    <!-- Section: Social media -->
+    <!-- Copyright -->
+    <div class="text-center p-4 text-secondary" style="background: #00337C">
+        Del Canteen Management System Copyright &copy; 2023 -
+        <script>
+            document.write(new Date().getFullYear())
+        </script> Your Name All Rights Reserved
+    </div>
+    <!-- Copyright -->
+</footer>
+
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,12 @@
 <style>
     #footer {
-        position: relative;
+        position:absolute
         bottom: 0;
         width: 100%;
         height: 60px;
         /* Height of the footer */
         background: #00337C;
+        font-family: sans-serif
     }
 </style>
 
