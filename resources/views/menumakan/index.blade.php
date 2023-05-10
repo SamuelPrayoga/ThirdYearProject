1edb43a63363348385afc4c680b587d3acfc78f3
Samuel Prayoga Tampubolon <samueltampubolon2002@gmail.com>
2023-05-10 20:41:22 +0700

    tabel menu

--- resources/views/menumakan/index.blade.php
+++ resources/views/menumakan/index.blade.php
@@ -5,57 +5,59 @@
             <div class="col-md-12">
                 <div class="card shadow-sm mb-2">
                     <div class="card-header">
-                        <center><img src="{{ asset('img/logo.png') }}" width="65px" alt="" srcset=""></center>
+                        {{-- <center><img src="{{ asset('img/logo.png') }}" width="65px" alt="" srcset=""></center> --}}
                         <center>DAFTAR MENU MAKANAN CIVITAS INSTITUT TEKNOLOGI DEL</center>
                     </div>
                     <div class="card-body">
-                        <table class="table table-bordered">
-                            <thead class="bg-light">
-                                <tr>
-                                    <th scope="row">Tanggal</th>
-                                    <th>
-                                        <center>Pagi</center>
-                                    </th>
-                                    <th>
-                                        <center>Siang</center>
-                                    </th>
-                                    <th>
-                                        <center>Malam</center>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @if ($menumakan->isEmpty())
-                                    <td colspan="5" class="table-inactive"><small>Belum ada Menu Makanan
-                                            ditambahkan</small></td>
-                                @else
-                                    @foreach ($menumakan as $menu)
-                                        <tr>
-                                            <td width="20%">{{ date('d M Y', strtotime($menu->tanggal_makan)) }}</td>
-                                            <td width="20%">
-                                                <center>{!! $menu->menu_pagi !!}</center>
-                                            </td>
-                                            <td width="20%">
-                                                <center>{!! $menu->menu_siang !!}</center>
-                                            </td>
-                                            <td width="20%">
-                                                <center>{!! $menu->menu_malam !!}</center>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @endif
-                            </tbody>
-                        </table>
+                        <div class="card mb-3 table-responsive">
+                            <table class="table table-bordered">
+                                <thead class="bg-light">
+                                    <tr>
+                                        <th scope="row">Tanggal</th>
+                                        <th>
+                                            <center>Pagi</center>
+                                        </th>
+                                        <th>
+                                            <center>Siang</center>
+                                        </th>
+                                        <th>
+                                            <center>Malam</center>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if ($menumakan->isEmpty())
+                                        <td colspan="5" class="table-inactive"><small>Belum ada Menu Makanan
+                                                ditambahkan</small></td>
+                                    @else
+                                        @foreach ($menumakan as $menu)
+                                            <tr>
+                                                <td width="20%">{{ date('d M Y', strtotime($menu->tanggal_makan)) }}</td>
+                                                <td width="20%">
+                                                    <center>{!! $menu->menu_pagi !!}</center>
+                                                </td>
+                                                <td width="20%">
+                                                    <center>{!! $menu->menu_siang !!}</center>
+                                                </td>
+                                                <td width="20%">
+                                                    <center>{!! $menu->menu_malam !!}</center>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @endif
+                                </tbody>
+                            </table>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <style>
-        .table-inactive {
-            background: #878787;
-            color: #858585;
-        }
-    </style>
-    @include('partials.footer')
-@endsection
+        <style>
+            .table-inactive {
+                background: #878787;
+                color: #858585;
+            }
+        </style>
+        @include('partials.footer')
+    @endsection
