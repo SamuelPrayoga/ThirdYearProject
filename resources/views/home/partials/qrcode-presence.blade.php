6254aa637012d1e8ed721408d55d16130d3ab51c
Samuel Prayoga Tampubolon <samueltampubolon2002@gmail.com>
2023-06-09 21:27:55 +0700

    attendance

--- resources/views/home/partials/qrcode-presence.blade.php
+++ resources/views/home/partials/qrcode-presence.blade.php
@@ -4,39 +4,22 @@
             <small class="fw-bold">Hari ini adalah hari libur.</small>
         </div>
     @else
+        {{-- kondisi untuk Hari Jumat --}}
         @if (in_array(now()->format('l'), ['Friday']) && $data['is_makan'] == 0)
             <div class="alert alert-danger">
-                <small class="fw-bold">Anda Tidak melaporkan Makan untuk Hari Jumat, sehingga Anda tidak boleh melakukan
-                    scan QR Code Masuk.</small>
+                <small class="fw-bold">Anda Tidak melaporkan Makan untuk Hari Jumat, sehingga Anda tidak boleh melakukan scan QR Code Masuk.</small>
             </div>
-        @endif
-
-        @if (in_array(now()->format('l'), ['Saturday']) && $data['is_makan'] == 0)
+        {{-- kondisi untuk Hari Sabtu --}}
+        @elseif (in_array(now()->format('l'), ['Saturday']) && $data['is_makan'] == 0)
             <div class="alert alert-danger">
-                <small class="fw-bold">Anda Tidak melaporkan Makan untuk Hari Sabtu, sehingga Anda tidak boleh melakukan
-                    scan QR Code Masuk.</small>
+                <small class="fw-bold">Anda Tidak melaporkan Makan untuk Hari Sabtu, sehingga Anda tidak boleh melakukan scan QR Code Masuk.</small>
             </div>
-        @endif
-
-        {{--
-        @if ($holiday)
-            <div class="alert alert-success">
-                <small class="fw-bold">Hari ini adalah hari libur.</small>
-            </div>
-        @elseif ($is_makan == 0 && in_array(now()->format('l'), ['Friday', 'Saturday']))
-            <div class="alert alert-danger">
-                <small class="fw-bold">Anda Tidak melaporkan Makan untuk Hari Jumat atau Sabtu, sehingga Anda tidak boleh
-                    melakukan scan QR Code Masuk.</small>
-            </div> --}}
-
-        {{-- jika tidak menggunakan qrcode (button) dan mahasiswa saat ini tidak menekan tombol izin --}}
-        @if ($attendance->data->is_using_qrcode && !$data['is_there_permission'])
+        {{-- kondisi untuk hari kerja --}}
+        @elseif ($attendance->data->is_using_qrcode && !$data['is_there_permission'])
             {{-- jika belum absen dan absen masuk sudah dimulai --}}
             @if ($attendance->data->is_start && !$data['is_has_enter_today'])
-                <button class="btn btn-primary px-3 py-2 btn-sm fw-bold d-block w-100 mb-2" data-bs-toggle="modal"
-                    data-bs-target="#qrcode-scanner-modal" data-is-enter="1">Scan QRCode Masuk</button>
-                <a href="{{ route('home.permission', $attendance->id) }}"
-                    class="btn btn-secondary  px-3 py-2 btn-sm fw-bold d-block w-100">Izin</a>
+                <button class="btn btn-primary px-3 py-2 btn-sm fw-bold d-block w-100 mb-2" data-bs-toggle="modal" data-bs-target="#qrcode-scanner-modal" data-is-enter="1">Scan QRCode Masuk</button>
+                <a href="{{ route('home.permission', $attendance->id) }}" class="btn btn-secondary  px-3 py-2 btn-sm fw-bold d-block w-100">Izin</a>
             @endif
 
             @if ($data['is_has_enter_today'])
@@ -47,15 +30,13 @@
 
             {{-- jika absen pulang sudah dimulai, dan mahasiswa sudah absen masuk dan belum absen pulang --}}
             @if ($attendance->data->is_end && $data['is_has_enter_today'] && $data['is_not_out_yet'])
-                <button class="btn btn-primary px-3 py-2 btn-sm fw-bold d-block w-100" data-bs-toggle="modal"
-                    data-bs-target="#qrcode-scanner-modal" data-is-enter="0">Scan QRCode Pulang</button>
+                <button class="btn btn-primary px-3 py-2 btn-sm fw-bold d-block w-100" data-bs-toggle="modal" data-bs-target="#qrcode-scanner-modal" data-is-enter="0">Scan QRCode Pulang</button>
             @endif
 
             {{-- sudah absen masuk dan absen pulang --}}
             @if ($data['is_has_enter_today'] && !$data['is_not_out_yet'])
                 <div class="alert alert-success">
-                    <small class="d-block fw-bold text-success">Terimakasih! Anda sudah makan dan Scan
-                        pulang, Semoga hari Anda menyenangkan.</small>
+                    <small class="d-block fw-bold text-success">Terimakasih! Anda sudah makan dan Scan pulang, Semoga hari Anda menyenangkan.</small>
                 </div>
             @endif
 
@@ -65,20 +46,19 @@
                     <small class="fw-bold">Jika Belum Selesai Makan, Belum saatnya melakukan Scan pulang.</small>
                 </div>
             @endif
-        @elseif($data['is_there_permission'])
+        {{-- kondisi untuk permintaan izin --}}
+        @elseif ($data['is_there_permission'])
             {{-- menampilkan pesan jika ada permintaan izin yang sedang diproses --}}
             @if ($data['is_permission_accepted'] == null)
                 <div class="alert alert-info">
                     <small class="fw-bold">Permintaan izin sedang diproses.</small>
                 </div>
-
-                {{-- menampilkan pesan jika permintaan izin ditolak --}}
+            {{-- menampilkan pesan jika permintaan izin ditolak --}}
             @elseif ($data['is_permission_accepted'] == 0)
                 <div class="alert alert-danger">
                     <small class="fw-bold">Permintaan izin Anda ditolak.</small>
                 </div>
-
-                {{-- menampilkan pesan jika permintaan izin diterima --}}
+            {{-- menampilkan pesan jika permintaan izin diterima --}}
             @else
                 <div class="alert alert-success">
                     <small class="fw-bold">Permintaan izin sudah diterima.</small>
@@ -104,6 +84,7 @@
         </div>
     </div>
 </div>
+
 
 @push('script')
     <script src="{{ asset('html5-qrcode/html5-qrcode.min.js') }}"></script>
